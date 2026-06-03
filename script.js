f6b9518b0fe8ba85247687adb3e3b3162784e877
Angela <angelacoreas1989@gmail.com>
2026-06-03 18:56:50 -0400

    Implement typing effect for text display

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio loaded 🚀");
+const text = "Building my future in tech...";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 80);
+  }
+}
+
+window.onload = typeEffect;
